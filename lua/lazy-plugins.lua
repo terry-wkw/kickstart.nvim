46c9ef4cc5e52ff59e13cb42b10a10dc5e39b7e2
Terry Wong <wong_terry@outlook.com>
2024-05-09 11:43:33 -0700

    Added nvim-tmux integration navigator

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -65,6 +65,8 @@ require('lazy').setup({
 
   require 'kickstart.plugins.bufferline',
 
+  require 'kickstart.plugins.nvim-tmux-navigator',
+
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
