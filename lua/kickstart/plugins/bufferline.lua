3a30008fb349975a9b2b0db322af77150796ffa0
Terry Wong <wong_terry@outlook.com>
2024-05-08 18:14:23 -0700

    Added bufferline tabbing plugin

---
+++ lua/kickstart/plugins/bufferline.lua
@@ -0,0 +1,49 @@
+return {
+  'akinsho/bufferline.nvim',
+  keys = {
+    { '<leader>blp', '<Cmd>BufferLineTogglePin<CR>', desc = 'Toggle Pin' },
+    { '<leader>blP', '<Cmd>BufferLineGroupClose ungrouped<CR>', desc = 'Delete Non-Pinned Buffers' },
+    { '<leader>bldr', '<Cmd>BufferLineCloseRight<CR>', desc = 'Delete Buffers to the Right' },
+    { '<leader>bldl', '<Cmd>BufferLineCloseLeft<CR>', desc = 'Delete Buffers to the Left' },
+    { '<S-h>', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
+    { '<S-l>', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
+  },
+  opts = {
+    options = {
+      mode = 'buffers',
+      -- stylua: ignore
+      close_command = function(n) require("mini.bufremove").delete(n, false) end,
+      -- stylua: ignore
+      right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
+      indicator = {
+        style = 'underline',
+      },
+      separator_style = 'slant',
+      always_show_bufferline = true,
+      diagnostics_indicator = function(_, _, diag)
+        local icons = require('lazyvim.config').icons.diagnostics
+        local ret = (diag.error and icons.Error .. diag.error .. ' ' or '') .. (diag.warning and icons.Warn .. diag.warning or '')
+        return vim.trim(ret)
+      end,
+      offsets = {
+        {
+          filetype = 'neo-tree',
+          text = 'Explorer',
+          highlight = 'Directory',
+          separator = true,
+        },
+      },
+    },
+  },
+  config = function(_, opts)
+    require('bufferline').setup(opts)
+    -- Fix bufferline when restoring a session
+    vim.api.nvim_create_autocmd('BufAdd', {
+      callback = function()
+        vim.schedule(function()
+          pcall(nvim_bufferline)
+        end)
+      end,
+    })
+  end,
+}
--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -63,6 +63,8 @@ require('lazy').setup({
   -- require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
 
+  require 'kickstart.plugins.bufferline',
+
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
